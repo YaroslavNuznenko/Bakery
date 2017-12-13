f51743b88626fa7ce001ad88d3f78f7d036d58d6
YaroslavNuznenko <nuznenko@gmail.com>
2017-12-13 21:50:12 +0200

    v2.0.1

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,7 +17,32 @@ $('.slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     nextArrow: '<div class="slider__nextArrow slider__Arrows"><i class="fa fa-angle-right slider__chevron" aria-hidden="true"></i></div>',
-    prevArrow: '<div class="slider__prevArrow slider__Arrows"><i class="fa fa-angle-left slider__chevron" aria-hidden="true"></i></div>'
+    prevArrow: '<div class="slider__prevArrow slider__Arrows"><i class="fa fa-angle-left slider__chevron" aria-hidden="true"></i></div>',
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                arrows: false
+            }
+        },
+        {
+            breakpoint: 370,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false
+            }
+        }
+    ]
 });
 
 $('.verticalSlider').slick({
@@ -28,4 +53,39 @@ $('.verticalSlider').slick({
     verticalSwiping: true,
     arrows: false,
     // autoplay: true
-});	
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                verticalSwiping: false,
+                vertical: false,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 700,
+            settings: {
+                verticalSwiping: false,
+                vertical: false,
+                slidesToShow: 1,
+                slidesToScroll:1
+            }
+        },
+    ]
+});
+
+
+/*Show Menu >992px*/
+$('.headerNav__hamburger').on("click", function () {
+    if($('.headerNav__collapsedMenu').hasClass('active')){
+        $('.headerNav__collapsedMenu').removeClass('active');
+        $('body').removeClass('fixed');
+        $('.hamburger--spring').removeClass('is-active');
+    }
+    else{
+        $('.headerNav__collapsedMenu').addClass('active');
+        $('body').addClass('fixed');
+        $('.hamburger--spring').addClass('is-active');
+    }
+})
